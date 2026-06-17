e3e9dd237841d596d0809dafde292526b9082275
Ashik275 <karzon275@gmail.com>
2026-06-17 15:09:00 +0600

    loop condition change

--- Array/LeetcodeProblems/Problem15/leetcodeProblem15.js
+++ Array/LeetcodeProblems/Problem15/leetcodeProblem15.js
@@ -1,16 +1,14 @@
 function findingTriplets(numbers) {
     const result = [];
     const seen = new Set();
-    for (let i = 0; i < numbers.length - 2; i++) {
-        for (let j = i + 1; j < numbers.length - 1; j++) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
             for (let k = j + 1; k < numbers.length; k++) {
 
                 const triplet = [numbers[i], numbers[j], numbers[k]];
                 const sum = numbers[i] + numbers[j] + numbers[k];
                 if (sum === 0) {
                     const key = [...triplet].sort((a, b) => a - b).join(",");
-                    console.log(seen);
-                    
                     
                     if (!seen.has(key)) {
                         seen.add(key);
@@ -24,6 +22,6 @@ function findingTriplets(numbers) {
 }
 
 
-// console.log(
+console.log(
     findingTriplets([-1, 0, 1, 2, -1, -4])
-// );
+);
